8ad765459e5e76f5b86b24d6251bdd406b72a590
SergeyZu <vechniystudent19@yandex.ru>
2023-04-12 16:58:19 +0700

    Добавил CurrentUserContext в Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card(props) {
-    function handleClick() {
-        props.onCardClick(props.card);
-    }
-    return (
-        <li className="card">
-            <img
-                className="card__image"
-                src={props.card.link}
-                alt={props.card.name}
-                onClick={handleClick}
-            />
-            <button className="card__trash" type="button"></button>
-            <div className="card__bottom">
-                <h2 className="card__title">{props.card.name}</h2>
-                <div className="card__like-block">
-                    <button
-                        className="card__like card__liked"
-                        type="button"
-                    ></button>
-                    <div className="card__likes-qty">
-                        {props.card.likes.length}
-                    </div>
-                </div>
-            </div>
-        </li>
-    );
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = props.card.owner._id === currentUser._id;
+  const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
+  const cardLikeButtonClassName = `card__like ${isLiked && "card__liked"}`;
+
+  function handleClick() {
+    props.onCardClick(props.card);
+  }
+
+  function handleDeleteClick() {}
+
+  return (
+    <li className="card">
+      <img
+        className="card__image"
+        src={props.card.link}
+        alt={props.card.name}
+        onClick={handleClick}
+      />
+      <button
+        className={`card__trash ${!isOwn && "card__hide-trash"}`}
+        type="button"
+        onClick={handleDeleteClick}
+      ></button>
+      <div className="card__bottom">
+        <h2 className="card__title">{props.card.name}</h2>
+        <div className="card__like-block">
+          <button className="card__like card__liked" type="button"></button>
+          <div className="card__likes-qty">{props.card.likes.length}</div>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default Card;
